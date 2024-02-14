70be75c0ecf828a0a0db137e40593532969187aa
Tomas <vidalltomas@gmail.com>
2024-02-14 20:21:21 -0300

    add delete button to tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { FormEvent, useState } from "react"
 import "./index.css";
 
+type Tab = {
+  url: string,
+  id: Id,
+}
+
+type Id = `${string}-${string}-${string}-${string}-${string}` 
+
 
 function App() {
   const [input, setInput] = useState<string>("");
-  const [tabs, setTabs] = useState<string[]>([])
+  const [tabs, setTabs] = useState<Tab[]>([])
   function openTabs() : void {
     tabs.forEach( async (tab) => {
       await chrome.tabs.create({
-        url: tab,
+        url: tab.url,
       });
     })
   }
@@ -18,30 +25,33 @@ function App() {
     if (!input) return;
     setInput("");
     if (input.includes("http://") || input.includes("https://")) {
-      setTabs( prevTabs => [...prevTabs, input]);
-    } else if (input.includes("wwww.")) {
-      setTabs( prevTabs => [...prevTabs, "https://" + input]);
+      setTabs( prevTabs => [...prevTabs, {url: input, id: crypto.randomUUID()}]);
+    } else if (input.includes("www.")) {
+      setTabs( prevTabs => [...prevTabs,{url: "https://" + input, id: crypto.randomUUID()}]);
     } else {
-      setTabs( prevTabs => [...prevTabs, "https://www." + input]);
+      setTabs( prevTabs => [...prevTabs, {url: "https://www." + input, id: crypto.randomUUID()}]);
     }
   }
 
-  function deleteTab(id) {
-
+  function deleteTab(id : Id) {
+    setTabs( (prevTabs : Tab[]) => {
+      return prevTabs.filter( (tab : Tab) => tab.id !== id)
+    })
   }
+
   return (
     <main className="w-[400px] h-[400px] bg-darker-gray flex flex-col items-center py-2">
       <form className="flex gap-2">
       <input className="px-2 rounded-xs" type="text" name="url" value={input} onChange={(evt) => setInput(evt.currentTarget.value)} placeholder="Enter new URL to save..." />
       <button className="bg-dark-gray text-white px-2 rounded-xs" type="submit" onClick={(e) => addTab(e)}>Add</button>
       </form>
-      <section className="flex-1 self-stretch my-2">
+      <section className="flex-1 self-stretch my-2 overflow-auto">
           <ul className="flex flex-col gap-2">
-            {tabs.map( (tab, idx) => {
+            {tabs.map( (tab) => {
               return(
               <li className="bg-dark-gray text-white/50 flex px-2">
-                {tab}
-                <button className="text-red-400 ml-auto" onClick={() => deleteTab()}>X</button>
+                {tab.url}
+                <button className="text-red-400 ml-auto" onClick={() => deleteTab(tab.id)}>X</button>
               </li>
               )
             })}
