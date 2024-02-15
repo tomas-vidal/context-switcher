5295b482d338e09dc4afeebf49630fa49ff389e0
Tomás Vidal <103294839+tomas-vidal@users.noreply.github.com>
2024-02-15 11:59:49 -0300

    add chrome storage hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,92 @@
-import { FormEvent, useState } from "react"
+import { FormEvent, useState } from "react";
+import { useChromeStorageLocal } from "use-chrome-storage";
 import "./index.css";
 
-type Tab = {
-  url: string,
-  id: Id,
-}
+export type Tab = {
+  url: string;
+  id: Id;
+};
 
-type Id = `${string}-${string}-${string}-${string}-${string}` 
-
+type Id = `${string}-${string}-${string}-${string}-${string}`;
 
 function App() {
   const [input, setInput] = useState<string>("");
-  const [tabs, setTabs] = useState<Tab[]>([])
-  function openTabs() : void {
-    tabs.forEach( async (tab) => {
+  const [tabs, setTabs] = useChromeStorageLocal<Tab[]>("tabs", []);
+
+  function openTabs(): void {
+    tabs.forEach(async (tab: Tab) => {
       await chrome.tabs.create({
         url: tab.url,
       });
-    })
+    });
   }
 
-  function addTab(e : FormEvent) : void {
+  async function addTab(e: FormEvent) {
     e.preventDefault();
     if (!input) return;
-    setInput("");
+    let url: string;
+    const id: Id = crypto.randomUUID();
     if (input.includes("http://") || input.includes("https://")) {
-      setTabs( prevTabs => [...prevTabs, {url: input, id: crypto.randomUUID()}]);
+      url = input;
     } else if (input.includes("www.")) {
-      setTabs( prevTabs => [...prevTabs,{url: "https://" + input, id: crypto.randomUUID()}]);
+      url = "https://" + input;
     } else {
-      setTabs( prevTabs => [...prevTabs, {url: "https://www." + input, id: crypto.randomUUID()}]);
+      url = "https://www." + input;
     }
+    setTabs((prevTabs) => [...prevTabs, { url, id }]);
+    setInput("");
   }
 
-  function deleteTab(id : Id) {
-    setTabs( (prevTabs : Tab[]) => {
-      return prevTabs.filter( (tab : Tab) => tab.id !== id)
-    })
+  async function deleteTab(id: Id) {
+    setTabs((prevTabs: Tab[]): Tab[] => {
+      return prevTabs.filter((tab: Tab) => tab.id !== id);
+    });
   }
 
   return (
     <main className="w-[400px] h-[400px] bg-darker-gray flex flex-col items-center py-2">
       <form className="flex gap-2">
-      <input className="px-2 rounded-xs" type="text" name="url" value={input} onChange={(evt) => setInput(evt.currentTarget.value)} placeholder="Enter new URL to save..." />
-      <button className="bg-dark-gray text-white px-2 rounded-xs" type="submit" onClick={(e) => addTab(e)}>Add</button>
+        <input
+          className="px-2 rounded-xs"
+          type="text"
+          name="url"
+          value={input}
+          onChange={(evt) => setInput(evt.currentTarget.value)}
+          placeholder="Enter new URL to save..."
+        />
+        <button
+          className="bg-dark-gray text-white px-2 rounded-xs"
+          type="submit"
+          onClick={(e) => addTab(e)}
+        >
+          Add
+        </button>
       </form>
       <section className="flex-1 self-stretch my-2 overflow-auto">
-          <ul className="flex flex-col gap-2">
-            {tabs.map( (tab) => {
-              return(
+        <ul className="flex flex-col gap-2">
+          {tabs.map((tab: Tab) => {
+            return (
               <li className="bg-dark-gray text-white/50 flex px-2">
                 {tab.url}
-                <button className="text-red-400 ml-auto" onClick={() => deleteTab(tab.id)}>X</button>
+                <button
+                  className="text-red-400 ml-auto"
+                  onClick={() => deleteTab(tab.id)}
+                >
+                  X
+                </button>
               </li>
-              )
-            })}
-          </ul>
+            );
+          })}
+        </ul>
       </section>
       <div>
-      <button
-        className="bg-red-danger rounded-sm tracking-normal py-2 px-10 font-semibold text-white"
-        onClick={openTabs}
-      >
-        Open tabs
-      </button></div>
+        <button
+          className="bg-red-danger rounded-sm tracking-normal py-2 px-10 font-semibold text-white"
+          onClick={openTabs}
+        >
+          Open tabs
+        </button>
+      </div>
     </main>
   );
 }
